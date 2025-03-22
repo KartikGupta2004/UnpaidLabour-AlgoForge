6becda9728e0e3b9844ef49d2afe7874c84e2839
Vedant Mehra <vedantmehra20@gmail.com>
2025-03-22 19:13:32 +0530

    foodhero v2 - included ml features and transaction logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,30 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import FaceRecognition from './components/FaceRecognition';
+// import Chatbot  from './components/ChatBot';
+// import ConfirmButtons from './components/TestOrderConfirm';
+// // import PriceAI from './components/PriceAI';
+// import PageOne from './components/PageOne';
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div className="App">
+//       <PageOne/>
+//       {/* <FaceRecognition /> */}
+//       {/* <Chatbot/> */}
+//       {/* <FaceRecognition /> */}
+//       {/* <ConfirmButtons transactionId="67dd9d68d2867db3f7559abe" serverUserId="67dd9d21d2867db3f7559abd" receiverUserId="67dd9c2fd2867db3f7559abc" /> */}
+//       </div>
+     
+//     </>
+//   )
+// }
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Home from "./pages/Home";
 import Login from "./pages/Login";
