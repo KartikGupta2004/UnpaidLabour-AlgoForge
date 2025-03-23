493ff3fbe82b58353ea2031c4fdca0ff7895890a
labhansh naik <labhanshvnaik2104@gmail.com>
2025-03-23 10:19:49 +0530

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -61,7 +61,7 @@ function Navbar() {
                       localStorage.removeItem("userType");
                       navigate("/");
                     }}
-                    className="rounded-full bg-blue-400 text-white px-4 py-2 text-sm hover:cursor-pointer"
+                    className="rounded-full bg-gray-600 text-white px-4 py-2 text-sm font-medium hover:cursor-pointer"
                   >
                     Logout
                   </button>
